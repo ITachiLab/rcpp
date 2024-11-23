1677f47cb2c7459d89f2fef1168845d5fb5889db
Itachi <github@itachi.pl>
2024-11-24 00:43:23 +0100

    Adapt ShallowHtmlArchiver to changed visitor interface.

--- src/archiver/ShallowHtmlArchiver.js
+++ src/archiver/ShallowHtmlArchiver.js
@@ -4,8 +4,7 @@ class ShallowHtmlArchiver extends Archiver {
   constructor() {
     super();
     this.doc = document.implementation.createHTMLDocument("Backup");
-    this.htmlStyle = 'body { font-family: sans-serif; } * { font-size: 14px; } table { width: 50%; margin-bottom: 20px; border-collapse: collapse; background-color: #eee; } table td { border: 1px solid; padding: 8px; } table td.date { text-align: right; width: 0; } table td.author { font-weight: bold; } .post-image { width: 300px; height: auto; } pre.snippet { border: dashed 1px grey; padding: 10px; background-color: #f8f8f8; } table tr.reply { box-shadow: 3px 0px 0px 0px black inset; } table td.message-content { white-space: pre-wrap; }';
-    this.trClass = "";
+    this.htmlStyle = 'body { font-family: sans-serif; } * { font-size: 14px; } table { width: 50%; margin-bottom: 20px; border-collapse: collapse; background-color: #eee; } table td { border: 1px solid; padding: 8px; } table td.date { text-align: right; width: 200px; } table td.author { font-weight: bold; } .post-image { width: 300px; height: auto; } pre.snippet { border: dashed 1px grey; padding: 10px; background-color: #f8f8f8; } table tr.reply { box-shadow: 3px 0px 0px 0px black inset; } table td.message-content { white-space: pre-wrap; }';
   }
 
   getBlob() {
@@ -13,72 +12,42 @@ class ShallowHtmlArchiver extends Archiver {
         {type: "text/html;charset=utf-8"}));
   }
 
-  enterConversation() {
-    let style = document.createElement("style");
+  visitConversation() {
+    const style = document.createElement("style");
     style.textContent = this.htmlStyle;
     this.doc.head.appendChild(style);
   }
 
-  enterMessage() {
+  visitMessage(msg) {
     this.table = this.doc.createElement("table");
-  }
-
-  exitMessage() {
     this.doc.body.appendChild(this.table);
+
+    this.__initMessageEntry();
+    this.__appendMessageContent(msg);
   }
 
-  enterReply() {
-    this.trClass = "reply";
-  }
+  visitReply(reply) {
+    this.__initMessageEntry();
 
-  exitReply() {
-    this.trClass = "";
+    this.infoRow.className = "reply";
+    this.contentRow.className = "reply";
+
+    this.__appendMessageContent(reply);
   }
 
   visitAuthor(profile) {
-    this.entryHeader = this.doc.createElement("tr");
-    this.entryHeader.className = this.trClass;
-    let author = this.doc.createElement("td");
-    author.textContent = profile.display_name;
-    author.className = "author";
-
-    this.entryHeader.appendChild(author);
-    this.table.appendChild(this.entryHeader);
-  }
-
-  visitDate(date) {
-    let dateCell = this.doc.createElement("td");
-    dateCell.textContent = new Date(date).toLocaleString();
-    dateCell.className = "date";
-    this.entryHeader.appendChild(dateCell);
-  }
-
-  visitMessageContent(content) {
-    let contentRow = this.doc.createElement("tr");
-    contentRow.className = this.trClass;
-
-    let contentCell = this.doc.createElement("td");
-    contentCell.colSpan = 2;
-    contentCell.className = "message-content";
-
-    this.msgContainer = this.doc.createElement("div");
-
-    let msgContent = this.doc.createElement("span");
-    msgContent.textContent = content;
-
-    this.msgContainer.appendChild(msgContent);
-    contentRow.appendChild(contentCell);
-    contentCell.appendChild(this.msgContainer);
-    this.table.appendChild(contentRow);
+    this.authorCell.textContent = profile.display_name;
+    this.authorCell.className = "author";
   }
 
   visitItemFile(url, name, fileType) {
-    let link = document.createElement("a");
+    const link = document.createElement("a");
     link.href = url;
+    link.target = "_blank";
     this.msgContainer.appendChild(link);
 
     if (fileType === "png" || fileType === "jpg" || fileType === "jpeg") {
-      let image = document.createElement("img");
+      const image = document.createElement("img");
       image.src = url;
       image.className = "post-image";
       link.appendChild(image);
@@ -88,13 +57,51 @@ class ShallowHtmlArchiver extends Archiver {
   }
 
   visitItemCodeSnippet(name, content, mimeType) {
-    let pre = document.createElement("pre");
-    let code = document.createElement("code");
+    const pre = document.createElement("pre");
+    const code = document.createElement("code");
     pre.appendChild(code);
 
     pre.className = "snippet";
     code.innerText = content;
     this.msgContainer.appendChild(pre);
+  }
+
+  __appendMessageContent(msg) {
+    this.dateCell.textContent = new Date(msg.created_at).toLocaleString();
+    this.dateCell.className = "date";
+
+    let content;
+    if (msg.hasOwnProperty("activity")) {
+      content = msg.activity;
+    } else {
+      content = msg.text;
+    }
+
+    const msgContent = this.doc.createElement("span");
+    msgContent.textContent = content;
+
+    this.msgContainer.appendChild(msgContent);
+  }
+
+  __initMessageEntry() {
+    this.infoRow = this.doc.createElement("tr");
+    this.contentRow = this.doc.createElement("tr");
+    this.msgContainer = this.doc.createElement("div");
+
+    this.contentCell = this.doc.createElement("td");
+    this.contentCell.colSpan = 2;
+    this.contentCell.className = "message-content";
+
+    this.authorCell = this.doc.createElement("td");
+    this.infoRow.appendChild(this.authorCell);
+
+    this.dateCell = this.doc.createElement("td");
+    this.infoRow.appendChild(this.dateCell);
+
+    this.table.appendChild(this.infoRow);
+    this.table.appendChild(this.contentRow);
+    this.contentRow.appendChild(this.contentCell);
+    this.contentCell.appendChild(this.msgContainer);
   }
 }
 
