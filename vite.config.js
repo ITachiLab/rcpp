1ebb7308163f4741af1b39f1597398f128a935cc
Itachi <github@itachi.pl>
2024-11-24 00:37:22 +0100

    Add linter configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import {defineConfig} from 'vite'
 import {resolve} from 'path'
+import {EsLinter, linterPlugin} from "vite-plugin-linter";
 
-export default defineConfig({
+export default defineConfig(configEnv => ({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/entry.js'),
       name: 'RCpp',
       fileName: 'rcpp'
     }
-  }
-})
+  },
+  plugins: [
+    linterPlugin({
+      include: ["./src/**/*.js"],
+      linters: [new EsLinter({ configEnv: configEnv })],
+    }),
+  ],
+}));
